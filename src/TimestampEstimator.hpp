9d9ff668aefb671abac84971e42531ea2583c4f8
Javier Hidalgo-Carrio <javier.hidalgo_carrio@dfki.de>
2018-02-20 18:34:00 +0100

    src: TimestampEstimator initial commit

---
+++ src/TimestampEstimator.hpp
@@ -0,0 +1,14 @@
+#ifndef STREAM_ALIGNER_TIMESTAMP_ESTIMATOR_HPP
+#define STREAM_ALIGNER_TIMESTAMP_ESTIMATOR_HPP
+
+#include "CircularArray.hpp"
+
+namespace stream_aligner
+{
+    class TimestampEstimator
+    {
+
+    };
+}
+
+#endif

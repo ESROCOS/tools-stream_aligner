220ca076c6bef7b011e06ad2e67a9a3b0bec169f
Javier Hidalgo-Carrio <javier.hidalgo_carrio@dfki.de>
2018-02-21 11:41:18 +0000

    cmake: enable test infrastructure

---
+++ test/test_streamaligner.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE "test_streamaligner"
+#define BOOST_AUTO_TEST_MAIN
+
+#include <iostream>
+#include <numeric>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/execution_monitor.hpp>  
+
+

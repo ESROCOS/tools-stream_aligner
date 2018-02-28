5b5d10d9f211a3e52fbb3264e4e60fb2e7987b40
Javier Hidalgo-Carrio <javier.hidalgo_carrio@dfki.de>
2018-02-28 16:52:26 +0000

    src: complete CircularArray

--- src/CircularArray.cpp
+++ src/CircularArray.cpp
@@ -2,43 +2,71 @@
 
 using namespace stream_aligner;
 
-void CircularArray::insert(const base::Time &ts)
+bool CircularArray::push(const base::Time &ts)
 {
-    if((rear == size-1 && front==0) || front==rear+1)
+    if(this->full())
     {
-        //std::cout<<"\nCircular queue is full";
-        return;
+        return false;
     }
     else if(rear == -1)
     {
         rear++;
         front++;
     }
-    else if(rear==size-1)
+    else if(rear==CircularArray::size-1)
         rear=0;
     else
         rear++;
 
     data[rear] = ts;
+    return true;
 }
 
-void CircularArray::remove()
+base::Time CircularArray::pop()
 {
-    if(front==-1)
+    base::Time ts =  base::Time::fromMicroseconds(0);
+
+    if(this->empty())
     {
-        //std::cout<<"\n Circular Queue is empty";
-        return;
+        return ts;
     }
 
     //std::cout<<data[front]<<" deleted"<<std::endl;
+    ts = data[front];
 
     if(front==rear)
     {
         front=-1;rear=-1;
     }
-    else if(front==size-1)
+    else if(front==CircularArray::size-1)
         front=0;
     else
         front++;
+
+    return ts;
 }
 
+bool CircularArray::empty()
+{
+    if(front==-1)
+    {
+        //std::cout<<"\n Circular Queue is empty";
+        return true;
+    }
+    return false;
+}
+
+bool CircularArray::full()
+{
+    if((rear == CircularArray::size-1 && front==0) || front==rear+1)
+    {
+        //std::cout<<"\nCircular queue is full";
+        return true;
+    }
+    return false;
+}
+
+size_t CircularArray::capacity()
+{
+    return CircularArray::size;
+}
--- src/CircularArray.hpp
+++ src/CircularArray.hpp
@@ -14,12 +14,59 @@ namespace stream_aligner
         std::array<base::Time,size> data;
         int front,rear;
     public:
+        /** @brief Constructor
+         *
+         *  @return void.
+         */
         CircularArray()
         {
             front=-1;rear=-1;
         }
-        void insert(const base::Time &ts);
-        void remove();
+
+        /** @brief insert an element
+         *
+         *  This methods insert a new element
+         *  in the CircularArray
+         *
+         *  @param ts the element.
+         *  @return void.
+         */
+        bool push(const base::Time &ts);
+
+
+        /** @brief remove an element
+         *
+         *  This methods remove an element
+         *  from the CircularArray
+         *
+         *  @param void.
+         *  @return void.
+         */
+        base::Time pop();
+
+        /** @brief array empty
+         *
+         *
+         *  @param void.
+         *  @return true if array is empty. false otherwise.
+         */
+        bool empty();
+
+        /** @brief array full
+         *
+         *
+         *  @param void.
+         *  @return true if array is full. false otherwise.
+         */
+        bool full();
+
+        /** @brief size
+         *
+         *
+         *  @param void.
+         *  @return fixed size of the array.
+         */
+        size_t capacity();
     };
 }
 #endif

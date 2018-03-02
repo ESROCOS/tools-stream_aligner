ea4460a6a9d200726ce4edb18ea9b727e4266358
Javier Hidalgo-Carrio <javier.hidalgo_carrio@dfki.de>
2018-03-02 16:45:59 +0000

    src: TimestampEstimator getPeriod implementation.

--- src/TimestampEstimator.cpp
+++ src/TimestampEstimator.cpp
@@ -6,7 +6,32 @@ TimestampEstimator::TimestampEstimator(TimestampConfig &initial_config)
 {
     samples.clear();
 
-    this->configuration =initial_config;
+    this->configuration = initial_config;
+    this->start_time = base::Time();
+    this->last_time = 0;
+}
+
+base::Time TimestampEstimator::getPeriod() const
+{
+    if (!this->samples.full() && this->configuration.period.toSeconds())
+    {
+        return this->configuration.period;
+    }
+    else
+    {
+        double latest = samples.back();
+        double earliest = samples.front();
+
+        if (base::isUnset(latest))
+        {
+            return this->configuration.period;
+        }
+        else if (base::isUnset(earliest))
+        {
+            return this->configuration.period;
+        }
+        return base::Time::fromSeconds((latest - earliest) / samples.size()-1);
+    }
 }
 
 TimestampStatus TimestampEstimator::getStatus() const
--- src/TimestampEstimator.hpp
+++ src/TimestampEstimator.hpp
@@ -11,15 +11,70 @@ namespace stream_aligner
     {
 
     protected:
+        /** To avoid loss of precision while manipulating doubles, we move all
+        * times to be relative to this time
+        *
+        * It gets added back when returning from update() and updateReference()
+        */
+        base::Time start_time;
+
+        /** The last estimated timestamp, without latency
+        *
+        * The current best estimate for the next sample, with no new
+        * information, is always last_time + getPeriod() - latency
+        */
+        double last_time;
+
+
+        /** Initial values **/
         TimestampConfig configuration;
+
+        /** Current status values **/
         TimestampStatus status;
-        CircularArray<> samples;
+
+        /** The buffer of samples **/
+        CircularArray<double> samples;
 
     public:
+        /** Constructor
+         * **/
         TimestampEstimator(TimestampConfig &initial_config);
 
+        /** Updates the estimate and return the actual timestamp for +ts+ */
+        base::Time update(base::Time ts);
+
+        /** Updates the estimate and return the actual timestamp for +ts+,
+        *  calculating lost samples from the index
+        */
+        base::Time update(base::Time ts, int64_t index);
+
+        /** The currently estimated period
+         * @return the period in case the buffer
+         * is full or the configuration period otherwise
+         * **/
+        base::Time getPeriod() const;
+
+        /** Returns the current latency estimate. This is valid only if
+         * updateReference() is called
+         */
+        base::Time getLatency() const;
+
+        /** Returns the maximum jitter duration estimated so far
+         *
+         * It is reset to zero only on reset
+         */
+        base::Time getMaxJitter() const;
+
+        /** The current status
+         * @return the status
+         * **/
         TimestampStatus getStatus() const;
 
+
+
+
+    protected:
+        bool push (const double time);
 
     };
 }

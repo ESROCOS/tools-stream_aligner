cf327a1113631bcbbecd2fd8c2006dc6669d3470
Javier Hidalgo-Carrio <javier.hidalgo_carrio@dfki.de>
2018-03-23 17:28:53 +0000

    src: complete Timestamp status class to report value about the Timestamp estimator

--- src/TimestampStatus.hpp
+++ src/TimestampStatus.hpp
@@ -11,16 +11,51 @@ namespace stream_aligner {
     {
         /** When this data structure has been generated */
         base::Time stamp;
-        /** Estimated period */
+
+        /** Last estimated period */
         base::Time period;
+
         /** Estimated latency (always null if no hardware timestamps are
          * provided)
          */
         base::Time latency;
 
+        /** Window of the Timestamp estimator **/
+        size_t window;
+
+        /** Count of samples currently stored in the estimator
+         * */
+        int lost_samples;
+
+        /** Total count of lost samples
+         */
+        int lost_samples_total;
+
+        /** Time at which the base time got reset last
+         */
+        base::Time base_time;
+        /** Offset at the last reset of base_time
+         */
+        base::Time base_time_reset_offset;
+
         /** Last input time provided to the input estimator
          */
         base::Time time_raw;
+
+        /** Last reference time provided to the input estimator
+         */
+        base::Time reference_time_raw;
+    };
+
+    std::ostream& operator << (std::ostream& stream, TimestampStatus const& status)
+    {
+        stream << "== Timestamp estimator status\n"
+            << "stamp: " << status.stamp.toString() << "\n"
+            << "period: " << status.period.toSeconds() << "\n"
+            << "latency: " << status.latency.toSeconds() << "\n"
+            << "lost_samples: " << status.lost_samples << "\n"
+            << "window: " << status.window << std::endl;
+        return stream;
     };
 }
 #endif

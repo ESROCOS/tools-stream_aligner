e3ce18e22671b7c7c460d577c64e679ee124c128
Javier Hidalgo-Carrio <javier.hidalgo_carrio@dfki.de>
2018-04-23 14:26:35 +0000

    test: two more unit_tests in stream_aligner.

--- test/test_streamaligner.cpp
+++ test/test_streamaligner.cpp
@@ -21,8 +21,9 @@ void test_callback( const base::Time &time, const std::string& sample )
 }
 
 
-BOOST_AUTO_TEST_CASE( order_test )
+BOOST_AUTO_TEST_CASE(order_test)
 {
+    std::cout<<"\n*** STREAM_ALIGNER [TEST 1] ***\n";
     stream_aligner::StreamAligner reader; 
     reader.setTimeout( base::Time::fromSeconds(2.0) );
 
@@ -40,11 +41,90 @@ BOOST_AUTO_TEST_CASE( order_test )
     reader.push<std::string, 4>(s2, base::Time::fromSeconds(3.0), std::string("d"));
     reader.push<std::string, 4>(s2, base::Time::fromSeconds(4.0), std::string("f"));
 
-    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK_EQUAL( last_sample, "a" );
-    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK_EQUAL( last_sample, "b" );
-    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK_EQUAL( last_sample, "c" );
-    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK_EQUAL( last_sample, "d" );
-    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK_EQUAL( last_sample, "e" );
-    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK_EQUAL( last_sample, "f" );
-    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK_EQUAL( last_sample, "" );
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK( last_sample == "a" );
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK( last_sample == "b" );
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK( last_sample == "c" );
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK( last_sample == "d" );
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK( last_sample == "e" );
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK( last_sample == "f" );
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK( last_sample == "" );
 }
+
+/** test case for enabling/disabling streams **/
+BOOST_AUTO_TEST_CASE(stream_enable_test)
+{
+    std::cout<<"\n*** STREAM_ALIGNER [TEST 2] ***\n";
+    StreamAligner reader; 
+    reader.setTimeout(base::Time::fromSeconds(2.0));
+
+    /** callback, period_time, (optional) priority **/
+    int s1 = reader.registerStream<std::string, 4>( &test_callback, base::Time::fromSeconds(1.0)); 
+    int s2 = reader.registerStream<std::string, 4>( &test_callback, base::Time::fromSeconds(0), 1);
+
+    reader.push<std::string, 4>( s1, base::Time::fromSeconds(1.0), std::string("a")); 
+    reader.push<std::string, 4>( s2, base::Time::fromSeconds(2.0), std::string("b")); 
+
+    BOOST_CHECK(reader.isStreamActive(s2) == true);
+    reader.disableStream(s2);
+    BOOST_CHECK(reader.isStreamActive(s2) == false);
+
+    /** we should still be able to read out the disabled stream **/
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK(last_sample == "a");
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK(last_sample == "b");
+
+    reader.push<std::string, 4>(s1, base::Time::fromSeconds(3.0), std::string("c"));
+
+    /** since s2 is disabled, we directly get c, without waiting for the timout **/
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK(last_sample == "c");
+
+    /** this should reenable s2 **/
+    reader.push<std::string, 4>(s2, base::Time::fromSeconds(4.0), std::string("d")); 
+    BOOST_CHECK(reader.isStreamActive(s2) == true);
+    reader.push<std::string, 4>(s1, base::Time::fromSeconds(5.0), std::string("e")); 
+
+    /** and we get the s2 sample **/
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK(last_sample == "d");
+
+    /** but have to wait for the next s1 sample, since timout is not over **/
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK(last_sample == "");
+}
+
+BOOST_AUTO_TEST_CASE(clear_test)
+{
+    std::cout<<"\n*** STREAM_ALIGNER [TEST 3] ***\n";
+    StreamAligner reader;
+    reader.setTimeout(base::Time::fromSeconds(2.0));
+
+    /** callback, period_time, (optional) priority **/
+    int s1 = reader.registerStream<std::string, 4>(&test_callback, base::Time::fromSeconds(2));
+    int s2 = reader.registerStream<std::string, 4>(&test_callback, base::Time::fromSeconds(2), 1);
+
+    reader.push<std::string, 4>(s1, base::Time::fromSeconds(1.0), std::string("a"));
+    reader.push<std::string, 4>(s1, base::Time::fromSeconds(3.0), std::string("c"));
+    reader.push<std::string, 4>(s2, base::Time::fromSeconds(2.0), std::string("b"));
+    reader.push<std::string, 4>(s2, base::Time::fromSeconds(3.0), std::string("d"));
+    reader.push<std::string, 4>(s2, base::Time::fromSeconds(4.0), std::string("f"));
+    reader.push<std::string, 4>(s1, base::Time::fromSeconds(4.0), std::string("e"));
+
+    reader.clear();
+
+    std::cout << reader.getStatus() << std::endl;
+
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK(last_sample == "");
+
+    reader.push<std::string, 4>(s1, base::Time::fromSeconds(1.0), std::string("a")); 
+    reader.push<std::string, 4>(s1, base::Time::fromSeconds(3.0), std::string("c")); 
+    reader.push<std::string, 4>(s2, base::Time::fromSeconds(2.0), std::string("b")); 
+    reader.push<std::string, 4>(s2, base::Time::fromSeconds(3.0), std::string("d")); 
+    reader.push<std::string, 4>(s2, base::Time::fromSeconds(4.0), std::string("f")); 
+    reader.push<std::string, 4>(s1, base::Time::fromSeconds(4.0), std::string("e")); 
+
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK(last_sample == "a");
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK(last_sample == "b");
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK(last_sample == "c");
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK(last_sample == "d");
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK(last_sample == "e");
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK(last_sample == "f");
+    last_sample = ""; reader.step<std::string, 4>(); BOOST_CHECK(last_sample == "" );
+}
+
